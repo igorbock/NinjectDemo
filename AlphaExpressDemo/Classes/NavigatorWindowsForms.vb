04c7db34af7a1960837f9ce6d7178e8b4eda1ffd
igorbdaluz <igorbdaluz@hotmail.com>
2024-09-13 20:57:42 -0300

    Alterando o método de buscar as telas para pesquisa em todos os projetos da solução. #26

--- AlphaExpressDemo/Classes/NavigatorWindowsForms.vb
+++ AlphaExpressDemo/Classes/NavigatorWindowsForms.vb
@@ -29,17 +29,19 @@ Public Class NavigatorWindowsForms
       Return form
     Next
 
-    Dim formName As String = $"AlphaAgroDemo.{pageName}"
-    Dim formType As Type = Assembly.GetAssembly(GetType(Exemplo)).GetType(formName)
-    If formType IsNot Nothing Then
-      Dim windowsForm As Form = _kernel.Get(formType)
-      If windowsForm IsNot Nothing Then
-        Return windowsForm
-      Else
-        Throw New Exception("Formulário não encontrado.")
-      End If
-    Else
-      Throw New Exception("Formulário não encontrado.")
-    End If
+    For Each asb As Assembly In AppDomain.CurrentDomain.GetAssemblies()
+      Try
+        For Each type As Type In asb.GetTypes()
+          If GetType(Form).IsAssignableFrom(type) AndAlso type.Name = pageName Then
+            Dim form As Form = CType(_kernel.Get(type), Form)
+            If form IsNot Nothing Then Return form
+          End If
+        Next
+      Catch ex As ReflectionTypeLoadException
+        MessageBox.Show($"Erro ao carregar tipos do assembly {asb.FullName}: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
+      End Try
+    Next
+
+    Throw New Exception("Formulário não encontrado.")
   End Function
 End Class
