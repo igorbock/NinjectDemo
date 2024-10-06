ccdb38d78bb5a64bac42edf1691b4c67cbef879c
igorbdaluz <igorbdaluz@hotmail.com>
2024-10-06 15:54:58 -0300

    Criando mensageiro próprio no AlphaLib. Após isso deverá ser criado a classe da mensagem com as propriedades devidas e a chamada nas ViewModels que vão compartilhar a troca de mensagens.

---
+++ AlphaLib/Messenger/AlphaMessenger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System;
+
+public class AlphaMessenger
+{
+    private static readonly AlphaMessenger _instance = new AlphaMessenger();
+    private readonly Dictionary<Type, List<Action<object>>> _subscribers = new Dictionary<Type, List<Action<object>>>();
+
+    private AlphaMessenger() { }
+
+    public static AlphaMessenger Instance => _instance;
+
+    public void Subscribe<T>(Action<T> action)
+    {
+        var key = typeof(T);
+        if (!_subscribers.ContainsKey(key))
+        {
+            _subscribers[key] = new List<Action<object>>();
+        }
+
+        _subscribers[key].Add(o => action((T)o));
+    }
+
+    public void Publish<T>(T message)
+    {
+        var key = typeof(T);
+        if (_subscribers.ContainsKey(key))
+        {
+            foreach (var action in _subscribers[key])
+            {
+                action(message);
+            }
+        }
+    }
+}
